bc64706d694a6d06e82d756e589f7914009e3668
Caio Silva <caiopacheco02@gmail.com>
2024-06-13 23:46:55 -0300

    Rebase

--- source/__init__.py
+++ source/__init__.py
@@ -1,8 +1,7 @@
-from threading import Thread
 from pynput.keyboard import Listener
 from time import sleep
-from win32api import mouse_event, SetCursorPos, GetSystemMetrics
-from win32con import MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP
+from win32api import mouse_event, GetSystemMetrics, keybd_event
+from win32con import MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, KEYEVENTF_KEYUP, VK_F1
 
 import gc
 gc.disable()
@@ -17,6 +16,7 @@ class CameraLockMacro(object):
         self.middle_y = int(0.5 * self.win_size[1])
         #self.favorite_key = keyboard.Key.f1
         self.favorite_key = "b"
+        self.target_key = VK_F1
         self.executing = False
         self.sleep = 0.0625 #ms
         self.rate = 0.00138888889 #720Hz
@@ -31,30 +31,19 @@ class CameraLockMacro(object):
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
     
     
-    def move_mouse(self) -> None:
-        self.executing = True
-        while self.executing:
-            SetCursorPos((self.x, self.y))
-            sleep(self.rate)
+    def press_release_key(self) -> None:
+        if not self.executing:
+            keybd_event(self.target_key, 0, 0, 0)
+            self.executing = True
+        else:
+            keybd_event(self.target_key, 0, KEYEVENTF_KEYUP, 0)
+            self.executing = False
             
-    
+
     def on_press(self, key) -> None:
         try:
             if key.char == self.favorite_key:
-                process = Thread(target=self.move_mouse)
-                process.daemon = True
-                process.start()
-                
-                sleep(self.sleep)
-                self.left_click()
-                sleep(self.sleep)
-                self.left_click()
-                
-                self.executing = False
-                sleep(self.sleep)
-                
-                SetCursorPos((self.middle_x, self.middle_y))
-                gc.collect()
+                self.press_release_key()
                 
         except AttributeError:
             pass        
